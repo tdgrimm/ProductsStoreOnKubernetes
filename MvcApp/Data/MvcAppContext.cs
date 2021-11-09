be7a966c381ca0b4a11dc9473c43cb69ed3b79c8
Terry Grimm <Terry.Grimm@neudesic.com>
2021-11-09 09:08:14 -0600

    first commit

---
+++ MvcApp/Data/MvcAppContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcApp.Models;
+
+namespace MvcApp.Data
+{
+    public class MvcAppContext : DbContext
+    {
+        public MvcAppContext(DbContextOptions<MvcAppContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("MvcAppContext", builder =>
+        //    {
+        //        builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        //    });
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+        public DbSet<Product> Product { get; set; }
+    }
+}
